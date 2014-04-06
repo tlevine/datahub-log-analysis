77c7cc377c9f7b41854a14798a53acd34b36d2ec
Thomas Levine <_@thomaslevine.com>
2014-04-06 16:27:26 +0000

    rename

--- nginx.hs
+++ nginx.hs
@@ -14,9 +14,8 @@ data HTTPVersion = V10 | V11 deriving Show
 
 data Scheme = HTTP | HTTPS deriving Show
 
-main = print $ parseOnly parseLogEntry "23.27.112.125 - - [02/Apr/2014:12:27:57 +0000] \"GET /user/register HTTP/1.0\" 200 4285 \"http://datahub.io/\" \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0.1\""
-data LogEntry =                                                                                                                                                        
-  LogEntry { ip :: IP
+data LogLine =                                                                                                                                                        
+  LogLine  { ip :: IP
            , day :: Day
            , timeOfDay :: TimeOfDay
            , method :: Method
