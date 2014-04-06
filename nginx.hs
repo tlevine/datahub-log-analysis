ec05e9f3a8c733b011bceda4d2a2f55e595b741b
Thomas Levine <_@thomaslevine.com>
2014-04-06 15:59:26 +0000

    -

--- nginx.hs
+++ nginx.hs
@@ -63,8 +63,9 @@ parseTimeOfDay = do
 
 parseLogEntry = do
   ip <- parseIP
-  string " - - ["  
-  return ip
+  string " - - ["
+  day <- parseDay
+  return day
 
 main :: IO ()
 main = print $ parseOnly parseLogEntry "23.27.112.125 - - [02/Apr/2014:12:27:57 +0000] \"GET /user/register HTTP/1.0\" 200 4285 \"http://datahub.io/\" \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0.1\""
