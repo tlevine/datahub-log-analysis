05e6bd2b47f66b0785fc706edd4491488b6f34a8
Thomas Levine <_@thomaslevine.com>
2014-04-06 15:55:27 +0000

    -

--- nginx.hs
+++ nginx.hs
@@ -64,6 +64,7 @@ parseTimeOfDay = do
   return $ TimeOfDay (read hour) (read minute) (read second)
 
 main :: IO ()
-main = print $ parseOnly parseDay "[02/Apr/2014:12:27:57 +0000] \"GET /user/register HTTP/1.0\" 200 4285 \"http://datahub.io/\" \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0.1\""
+main = print $ parseOnly parseDay "02/Apr/2014"
+--main = print $ parseOnly parseDay "[02/Apr/2014:12:27:57 +0000] \"GET /user/register HTTP/1.0\" 200 4285 \"http://datahub.io/\" \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0.1\""
 --main = print $ parseOnly parseMonth "Apr"
 --main = print $ parseOnly parseIP "23.27.112.125 - - [02/Apr/2014:12:27:57 +0000] \"GET /user/register HTTP/1.0\" 200 4285 \"http://datahub.io/\" \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0.1\""
