8cb3766f19423c7e9151a09a9d045f31f6d4556a
Thomas Levine <_@thomaslevine.com>
2014-04-06 15:59:12 +0000

    -

--- nginx.hs
+++ nginx.hs
@@ -12,8 +12,6 @@ import Control.Applicative
 -- | Type for IP's.
 data IP = IP Word8 Word8 Word8 Word8 deriving Show
 
--- data Date =
-
 data Method = GET | POST deriving Show
 
 data HTTPVersion = V10 | V11 deriving Show
@@ -63,8 +61,10 @@ parseTimeOfDay = do
   second <- count 2 digit
   return $ TimeOfDay (read hour) (read minute) (read second)
 
+parseLogEntry = do
+  ip <- parseIP
+  string " - - ["  
+  return ip
+
 main :: IO ()
-main = print $ parseOnly parseDay "02/Apr/2014"
---main = print $ parseOnly parseDay "[02/Apr/2014:12:27:57 +0000] \"GET /user/register HTTP/1.0\" 200 4285 \"http://datahub.io/\" \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0.1\""
---main = print $ parseOnly parseMonth "Apr"
---main = print $ parseOnly parseIP "23.27.112.125 - - [02/Apr/2014:12:27:57 +0000] \"GET /user/register HTTP/1.0\" 200 4285 \"http://datahub.io/\" \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0.1\""
+main = print $ parseOnly parseLogEntry "23.27.112.125 - - [02/Apr/2014:12:27:57 +0000] \"GET /user/register HTTP/1.0\" 200 4285 \"http://datahub.io/\" \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0.1\""
